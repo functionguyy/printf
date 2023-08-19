f9ce671fb79267f01f08d81cfcdc45044fc106f8
Gideon Azibaobuom Edoghotu <edoghotugiddy@gmail.com>
2023-08-19 13:48:55 +0000

    modified the file  to change 0 to \0

--- _printf.c
+++ _printf.c
@@ -16,7 +16,7 @@ int _printf(const char *format, ...)
 
 	va_start(args, format);
 
-	for (i = 0; format[i] != 0; i++)
+	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
 			_putchar(format[i]);
