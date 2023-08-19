a3021cbea3b3217149d8c65552aeebfa4b80bb71
Gilbert Adikankwu <gilbert945@outlook.com>
2023-08-19 04:28:58 +0100

    feat: add header file

---
+++ main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* C standard library header files */
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/* function prototypes */
+int _putchar(char c);
+int _printf(const char * const format, ...);
+int (*find_format_handlers(const char *format))(va_list arg);
+int print_spec_c_match(va_list arg);
+int print_spec_d_match(va_list arg);
+int print_spec_i_match(va_list arg);
+#if
+int print_spec_s_match(va_list arg);
+
+
+#endif /* MAIN_H */
