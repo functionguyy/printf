dd02240cf092a9482be1752249e477c83dd331ac
Gilbert Adikankwu <gilbert945@outlook.com>
2023-08-22 18:48:43 +0100

    fix: refactor code

--- print_decimal_specifier.c
+++ print_decimal_specifier.c
@@ -1,48 +1,5 @@
 #include "main.h"
 /**
- * print_number - function that prints a positive integer
- * @digit: a positive number
- *
- * Return: returns the number of characters printed to standard output
- */
-int print_number(unsigned int digit)
-{
-	/* declare variables */
-	unsigned int exponent, count, num, number;
-	int r_value;
-
-	/* initialize variables */
-	count = 0;
-	exponent = 1;
-	r_value = 0;
-	num = digit;
-
-	/* get the number of tens in the number */
-	while (digit > 0)
-	{
-		digit = digit / 10;
-		count++;
-	}
-
-	/* calculate its exponent */
-	while (count > 1)
-	{
-		exponent = exponent * 10;
-		count--;
-	}
-
-	/* print the integer */
-	while (exponent > 0)
-	{
-		number = num / exponent;
-		r_value += _putchar(number + '0');
-		num = num - (number * exponent);
-		exponent /= 10;
-	}
-
-	return (r_value);
-}
-/**
  * print_spec_d_match - function prints the variadic argument specified for the
  * format specifier d
  * @arg: variadic argument
@@ -53,11 +10,12 @@ int print_spec_d_match(va_list arg)
 {
 	/* declare variable */
 	int n, r_value;
-	unsigned int num;
+	unsigned int num, base;
 
 	/* initialize variables */
 	n = va_arg(arg, int);
 	r_value = 0;
+	base = 10;
 
 	/* account for negative number */
 	if (n < 0)
@@ -69,7 +27,7 @@ int print_spec_d_match(va_list arg)
 		num = n;
 
 	/* print the integer */
-	r_value += print_number(num);
+	r_value += print_number(num, base);
 
 	return (r_value);
 }
